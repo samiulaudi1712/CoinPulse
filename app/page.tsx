7468d6e3bd03ead59117d834182d5b727c1621bf
Samiul Islam Audi <samiul2304audi@gmail.com>
2026-02-25 02:59:17 +0600

    implemented navigation system

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const page = () => {
-  return <p className="text-3xl text-indigo-500">COINPULSE</p>
+  return <div></div>
 }
-
 export default page
---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+'use client'
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const Header = () => {
+    const pathname=usePathname();
+  return (
+    <header>
+        
+        <div className="main-container inner">
+
+    <Link href="/">
+
+        <img src="logo.svg" alt="CoinPulse logo" width={132} height={40} />
+
+    </Link>
+
+    <nav>
+        <Link href='/' className={cn('nav-link' , {'is-active': pathname ==='/','is-home':true})}>Home</Link>
+        
+        <p>Search Modal</p>
+        
+        <Link href="/coins" className={cn('nav-link' , {'is-active': pathname ==='/coins',})}>All Coins</Link>
+
+    </nav>
+
+
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
